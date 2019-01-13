1a16eeab98e296f78545fb36f2108111729e80cb
张新征 <programer.email@gmail.com>
2019-01-13 22:47:31 +0800

    增加喜欢页面

--- models/classic.js
+++ models/classic.js
@@ -29,6 +29,14 @@ export class ClassicModel extends HTTP{
     }
   }
 
+  getMyFavor (success) {
+    const params = {
+      url: 'classic/favor',
+      success: success
+    }
+    this.request(params)
+  }
+
   isFirst (index) {
     return index === 1 ? true : false
   }
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,18 +1,84 @@
 // pages/my/my.js
+import {ClassicModel} from '../../models/classic.js'
+import {BookModel} from '../../models/book.js'
+
+const classicModel = new ClassicModel()
+const bookModel = new BookModel()
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    authorized: false,
+    userInfo: null,
+    bookCount: 0,
+    classics: null
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    this.userAuthorized()
+    this.getMyBookCount()
+    this.getMyFavor()
+  },
 
+  getMyFavor () {
+    classicModel.getMyFavor(res => {
+      this.setData({
+        classics: res
+      })
+    })
+  },
+
+  getMyBookCount () {
+    bookModel.getMyBookCount()
+      .then(res => {
+        this.setData({
+          bookCount: res.count
+        })
+      })
+  },
+
+  userAuthorized () {
+    wx.getSetting({
+      success: data => {
+        if (data.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: data => {
+              this.setData({
+                authorized: true,
+                userInfo: data.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo (event) {
+    const userInfo = event.detail.userInfo
+    if (userInfo) {
+      this.setData({
+        userInfo,
+        authorized: true
+      })
+    }
+  },
+
+  onJumpToAbout (event) {
+    wx.navigateTo({
+      url: '/pages/about/about',
+    })
+  },
+
+  onStudy (event) {
+    wx.navigateTo({
+      url: '/pages/course/course',
+    })
   },
 
   /**
